952c3dbf262679e3cc00d699e3a3fd750dd2031b
Lucas de Castro <lucasdec88@gmail.com>
2026-09-02 15:41:40 +0100

    More DB Settings

--- src/main/java/br/com/alura/domain/Agencia.java
+++ src/main/java/br/com/alura/domain/Agencia.java
@@ -1,5 +1,6 @@
 package br.com.alura.domain;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -20,7 +21,7 @@ public class Agencia {
     @Column(name = "razao_social")
     private String razaoSocial;
     private String cnpj;
-    @OneToOne
+    @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "endereco_id")
     private Endereco endereco;
 
